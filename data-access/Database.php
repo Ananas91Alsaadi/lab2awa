d4f57c3b62a708d6cb7fc3518efb8f61696554b3
Ananas91Alsaadi <anas_al-saadi@hotmail.com>
2023-04-23 21:22:38 +0200

    4

--- data-access/Database.php
+++ data-access/Database.php
@@ -1,6 +1,9 @@
 <?php
 
-
+// Check for a defined constant or specific file inclusion
+if (!defined('MY_APP') && basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    die('This file cannot be accessed directly.');
+}
 
 // Data access:
 // Class for connecting to database
